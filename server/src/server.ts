38f5af433f169a75d922b8eb2dec860edbcfc365
JoseffJ <115510217+JoseffJ@users.noreply.github.com>
2024-12-11 00:28:29 -0600

    Update server.ts
    updated imports

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,11 @@ import dotenv from 'dotenv';
 import express from 'express';
 import path from 'path';
 import routes from './routes/api/weatherRoutes.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 
